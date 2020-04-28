de635e994d0e3fd514ee814a96b49184cf051d24
kbszabolcs <kbszabolcs@gmail.com>
2020-04-28 14:58:26 +0200

    SpongyaBazsi request fix

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -33,6 +33,7 @@ public class TorpedoStore {
   public boolean fire(int numberOfTorpedos){
     if(numberOfTorpedos < 1 || numberOfTorpedos > this.torpedoCount){
       throw new IllegalArgumentException("numberOfTorpedos");
+      
     }
 
     boolean success = false;
