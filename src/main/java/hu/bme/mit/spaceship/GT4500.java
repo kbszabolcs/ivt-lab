3315f56e18c88bb7a1e76f136f7564372fc7b851
kbszabolcs <kbszabolcs@gmail.com>
2020-05-19 17:31:16 +0200

    isolation

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -15,6 +15,11 @@ public class GT4500 implements SpaceShip {
     this.secondaryTorpedoStore = new TorpedoStore(10);
   }
 
+  public GT4500(TorpedoStore p, TorpedoStore s) {
+    this.primaryTorpedoStore = p;
+    this.secondaryTorpedoStore = s;
+  }
+
   public boolean fireLaser(FiringMode firingMode) {
     // TODO not implemented yet
     return false;
@@ -80,7 +85,7 @@ public class GT4500 implements SpaceShip {
         // try to fire both of the torpedo stores
         //TODO implement feature
     	  
-    	firingSuccess = primaryTorpedoStore.fire(1) && primaryTorpedoStore.fire(2);
+    	firingSuccess = primaryTorpedoStore.fire(1) && secondaryTorpedoStore.fire(1);
 
         break;
     }
--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -10,31 +10,44 @@ public class GT4500Test {
 
   private GT4500 ship;
 
+  private TorpedoStore mockPTS;
+  private TorpedoStore mockSTS;
+
   @BeforeEach
   public void init(){
-    this.ship = new GT4500();
+    mockPTS = mock(TorpedoStore.class);
+    mockSTS = mock(TorpedoStore.class);
+    this.ship = new GT4500(mockPTS,mockSTS);
   }
 
   @Test
   public void fireTorpedo_Single_Success(){
     // Arrange
+    when(mockPTS.fire(1)).thenReturn(true);
 
     // Act
     boolean result = ship.fireTorpedo(FiringMode.SINGLE);
 
     // Assert
     assertEquals(true, result);
+
+    verify(mockPTS, times(1)).fire(1);
   }
 
   @Test
   public void fireTorpedo_All_Success(){
     // Arrange
+    when(mockPTS.fire(1)).thenReturn(true);
+    when(mockSTS.fire(1)).thenReturn(true);
 
     // Act
     boolean result = ship.fireTorpedo(FiringMode.ALL);
 
     // Assert
     assertEquals(true, result);
+
+    verify(mockPTS, times(1)).fire(1);
+    verify(mockSTS, times(1)).fire(1);
   }
 
 }
